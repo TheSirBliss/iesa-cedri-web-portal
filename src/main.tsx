1ba886d74b227f8684225c0a8c1854369b3e136b
TheSirBliss <sialpa@libero.it>
2025-07-15 13:09:51 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async' // <-- 1. Importa
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. Avvolgi l'intera App nel Provider */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
+)
